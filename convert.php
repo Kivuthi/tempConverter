e1e70796e5d9c28acb47306640fee5545919a445
West Side <officialdee77@gmail.com>
2025-08-31 22:03:40 +0300

    update

--- convert.php
+++ convert.php
@@ -1,7 +1,15 @@
 <?php
-function convertTemperature($celcius) {
-    return ($celcius * 9/5) + 32;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $celcius = $_POST['temperature'];
+
+    function convertTemperature($celcius) {
+    return (($celcius * 9/5) + 32);
 }
-$result = convertTemperature(20);
-echo "<p>your temperature in Fahrenheit is: $result</p>";
+
+$result = convertTemperature($celcius);
+
+echo "<h2>Conversion Result";
+echo "<p>$celcius °C = $result °F</p>";
+echo '<a href="index.html">convert another</a>';
+}
 ?>
